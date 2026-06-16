9bff1304003c7507f60c37dad8e28cf25b7be079
m.tsuruoka <m.tsuruoka@core-tech.jp>
2026-06-16 14:12:04 +0900

    TODO表示とTODO追加機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [todos,setTodos] = useState([])
+  const [inputText, setInputText] = useState("")
+
+  // 追加関数（自分で実装）
+  const addTodo = () => {
+    if(inputText.trim() === "")return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      done: false,
+    }
+    setTodos([...todos, newTodo])
+    setInputText("")
+  }
+
   return (
     <div className="app">
       <h1>Todoアプリ</h1>
+      <AddTodoForm
+        inputText={inputText}
+        onInputChange={setInputText}
+        onAddTodo={addTodo}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
